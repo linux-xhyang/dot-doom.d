03a5ee3df9df3f7f2c561cfada359bbcc3553bfd
yangxiaohe <yangxiaohe@formovie.cn>
2022-05-08 20:10:49 +0800

    enable sp-kill-hybrid-sexp only prog-mode

--- install_fonts.sh
+++ install_fonts.sh
@@ -5,8 +5,6 @@ cd ~/.local/share/fonts/ || exit
 echo "Installing fonts..."
 
 url=https://www.1001fonts.com/download/jetbrains-mono.zip
-# https://download.jetbrains.com/fonts/JetBrainsMono-2.242.zip
-
 curl -L $url > JetBrainsMono.zip
 unzip -o JetBrainsMono.zip -d JetBrainsMono
 
@@ -18,8 +16,16 @@ url=https://www.1001fonts.com/download/alegreya.zip
 curl -L $url > Alegreya.zip
 unzip -o Alegreya.zip -d Alegreya
 
+url=https://www.wfonts.com/download/data/2014/06/01/microsoft-yahei/microsoft-yahei.zip
+curl -L $url > MicroYahei.rar
+unrar x MicroYahei.rar
+
+#url=http://x.downya.com/down/weiruanyaheibold_downyi.com.zip
+#curl -L $url > MicroYaheiBold.zip
+#unzip -o MicroYaheiBold.zip -d MicroYaheiBold
+
 fc-cache -f -v
 rm JetBrainsMono.zip
 rm Merriweather.zip
 rm Alegreya.zip
-
+rm MicroYahei.rar
